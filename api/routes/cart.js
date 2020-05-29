0de62c42cf4cbf748ec1dc22c9502656c65c713a
miq87 <miq3l@wp.pl>
2020-05-29 14:30:56 +0200

    ok

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -15,4 +15,24 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.post('/', (req, res, next) => {
+  const cart = new Cart({
+    productId: req.body._id,
+    productName: req.body.name,
+    price: req.body.price,
+    qty: 1
+  });
+  cart.save()
+  .then(result => {
+    res.status(200).json({
+      msg: 'Dodano nowy produkt',
+      Koszyk: cart
+    });
+  })
+  .catch(err => {
+    res.status(500).json({ blad:err })
+  });
+});
+
+
 module.exports = router;

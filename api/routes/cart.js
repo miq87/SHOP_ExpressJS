6c3b66a7e4b2f52a203069b339e0b51d7db7d448
miq87 <miq3l@wp.pl>
2020-06-02 15:28:58 +0200

    post update

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -16,27 +16,37 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const cart = new Cart({
-    //_id: mongoose.Types.ObjectId(),
-    productId: req.body.productId,
-    productName: req.body.productName,
-    price: req.body.price,
-    qty: 1
-  });
-  cart.save()
-  .then(result => {
-    res.status(200).json({
-      msg: 'Dodano nowy produkt',
-      Koszyk: cart
-    });
+  const id = req.body.productId
+  
+  const filter = { "productId": id };
+  Cart.findOneAndUpdate(filter, { $inc: { qty: 1 } }, { new: true })
+  .exec()
+  .then(doc => {
+    if(doc) {
+      res.status(200).json({ msg: 'Zwiększono ilość produktu o id ' + id, new: doc })
+    }
+    else {
+      if(!req.body.productName) req.body.productName = "Demo Name"
+      if(!req.body.price) req.body.price = 0
+
+      const cart = new Cart({
+        productId: req.body.productId,
+        productName: req.body.productName,
+        price: req.body.price,
+        qty: 1
+      })
+      cart.save()
+      .then(result => {
+        res.status(200).json({ msg: 'Dodano nowy produkt!', new: result })
+      })
+      .catch(err => {
+        res.status(500).json({ Error: err })
+      })
+    }
   })
   .catch(err => {
-    res.status(500).json({ 
-      Koszyk: cart,
-      blad: err
-    })
-  });
+    res.status(500).json({ Error: err })
+  })
 });
 
-
-module.exports = router;
+module.exports = router

f5ca36b6593c80ef48583a68e345c56404b26bbe
miq87 <miq3l@wp.pl>
2020-06-03 15:09:10 +0200

    add patch

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
 
-const Cart = require('../models/cart');
+const Cart = require('../models/cart')
 
 router.get('/', (req, res, next) => {
   Cart.find()
@@ -12,41 +12,98 @@ router.get('/', (req, res, next) => {
   })
   .catch(err => {
     res.status(500).json({ blad:err })
-  });
-});
+  })
+})
 
 router.post('/', (req, res, next) => {
   const id = req.body._id
   
-  const filter = { "_id": id };
-  Cart.findOneAndUpdate(filter, { $inc: { qty: 1 } }, { new: true })
+  Cart.findByIdAndUpdate(id, { $inc: { qty: 1 } }, { new: true })
   .exec()
   .then(doc => {
     if(doc) {
       res.status(200).json({ msg: 'Zwiększono ilość produktu o id ' + id, new: doc })
     }
-    else {
-      if(!req.body.productName) req.body.productName = "Demo Name"
-      if(!req.body.price) req.body.price = 0
-
-      const cart = new Cart({
-        _id: id,
-        productName: req.body.productName,
-        price: req.body.price,
-        qty: 1
-      })
-      cart.save()
-      .then(result => {
-        res.status(200).json({ msg: 'Dodano nowy produkt!', new: result })
-      })
-      .catch(err => {
-        res.status(500).json({ Error: err })
-      })
-    }
+    const cart = new Cart({
+      _id: id,
+      productName: req.body.productName,
+      price: req.body.price,
+      qty: 1
+    })
+    cart.save()
+    .then(result => {
+      res.status(200).json({ msg: 'Dodano nowy produkt!', new: result })
+    })
+    .catch(err => {
+      res.status(500).json({ Error: err })
+    })
   })
   .catch(err => {
     res.status(500).json({ Error: err })
   })
-});
+})
+
+router.patch('/', (req, res, next) => {
+  const id = req.body._id
+  const action = req.body.action
+
+  if(action === "inc") {
+    Cart.findByIdAndUpdate(id, { $inc: { qty: 1 } }, { new: true })
+    .exec()
+    .then(doc => {
+      if(doc) {
+        res.status(200).json({
+          msg: 'Zmieniono dane produktu nr ' + id,
+          data: doc
+        })
+      }
+      res.status(200).json({
+        msg: 'Nie ma produktu o nr ' + id,
+        data: doc
+      })
+    })
+    .catch(err => {
+      res.status(500).json({ blad: err })
+    })
+  }
+  else if(action === "dec") {
+    Cart.findByIdAndUpdate(id, { $inc: { qty: -1 } }, { new: true })
+    .exec()
+    .then(doc => {
+      if(doc) {
+        res.status(200).json({
+          msg: 'Zmieniono dane produktu nr ' + id,
+          data: doc
+        })
+      }
+      res.status(200).json({
+        msg: 'Nie ma produktu o nr ' + id,
+        data: doc
+      })
+    })
+    .catch(err => {
+      res.status(500).json({ blad: err })
+    })
+  }
+})
+
+router.delete('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  Cart.findByIdAndRemove(id)
+  .exec()
+  .then(doc =>{
+    if(doc) {
+      res.status(200).json({
+        msg: 'Usunięto produkt z koszyka o id ' + id
+      })
+    }
+    res.status(200).json({
+      msg: 'Nie ma już produktu w koszyku o id ' + id
+    })
+  })
+  .catch(err => {
+    res.status(500).json({ Error: err })
+  })
+})
 
 module.exports = router
